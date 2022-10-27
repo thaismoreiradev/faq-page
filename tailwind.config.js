42e0e742d0e76f598df07664e4554f4237a87668
thaismoreiradev <contatothaismoreira@hotmail.com>
2022-10-26 22:25:19 -0300

    tailwind colors & sizes details

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
+    screens: {
+      'xs': '455px'
+    },
+    fontFamily: {
+      'anton': ['Anton', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
